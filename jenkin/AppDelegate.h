432880f51496a4f15b631f9c3eaddf029f1a3101
saroj swain <saroja.swain@accionlabs.com>
2016-08-23 00:36:21 +0530

    initial

---
+++ jenkin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jenkin
+//
+//  Created by Saroj Swain on 23/08/16.
+//  Copyright © 2016 saroj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jenkin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkin
+//
+//  Created by Saroj Swain on 23/08/16.
+//  Copyright © 2016 saroj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
